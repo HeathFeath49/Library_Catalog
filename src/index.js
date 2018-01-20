1196a918ac23cfa678f6d63685857de24924ac50
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-01-20 12:27:38 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import LoginForm from './js/Components/loginForm';
+import LoginForm from './js/components/loginForm';
+import MainHeader from './js/components/mainHeader';
 
 
-function LargeText(props){
-	return <h1>{props.text}</h1>
-}
 
-function Welcome(){
-	return(
-		<div id='welcome'>
-			<LargeText text = 'Welcome!'/>
-		</div>
-	);
-}
 
 function App(){
 	return (
 		<div>
-			<Welcome />			
+			<MainHeader />			
 			<LoginForm />
 		</div>
 	);
