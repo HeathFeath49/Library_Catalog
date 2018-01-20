600f781df30073570155cf4543323da4dd0ebb70
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-01-19 20:49:35 -0500

    first practice components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './css/index.css';
+import LoginForm from './js/Components/loginForm';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+function LargeText(props){
+	return <h1>{props.text}</h1>
+}
+
+function Welcome(){
+	return(
+		<div id='welcome'>
+			<LargeText text = 'Welcome!'/>
+		</div>
+	);
+}
+
+function App(){
+	return (
+		<div>
+			<Welcome />			
+			<LoginForm />
+		</div>
+	);
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
