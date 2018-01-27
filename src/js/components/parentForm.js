63fb6f70ead0262e80c5890bc894b262c178ce17
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-01-27 13:51:14 -0500

    fixed infinite loops error

--- src/js/components/parentForm.js
+++ src/js/components/parentForm.js
@@ -7,15 +7,13 @@ class ParentForm extends React.Component{
 
 	constructor(props){
 		super(props);
+		this.state = {
+			signUpSelected: true,
+			loginSelected: false
+		};
 		this.updateState = this.updateState.bind(this);
 	}
 
-	getInitialState(){
-		return{ 
-			signUpSelected: true,
-			loginSelected: false
-		}
-	}
 
 	updateState(selectedBtn){
 		var loginSelected, signUpSelected;
@@ -42,8 +40,8 @@ class ParentForm extends React.Component{
 		return(
 			<div>
 				<div id='buttons'>
-					<div id='signUpBtn' onClick={this.updateState('signUp')} className={this.props.signUpSelected ? "selectedBtn" : "notSelectedBtn"}>Sign Up</div>
-					<div id='loginBtn' onClick={this.updateState('login')} className={this.props.loginSelected ? "selectedBtn":"notSelectedBtn"}>Login</div>
+					<div id='signUpBtn' onClick={()=>this.updateState('signUp')} className={this.state.signUpSelected ? "selectedBtn" : "notSelectedBtn"}>Sign Up</div>
+					<div id='loginBtn' onClick={()=>this.updateState('login')} className={this.state.loginSelected ? "selectedBtn":"notSelectedBtn"}>Login</div>
 				</div>
 			</div>
 			//return form needed based on state of clicked buttons
