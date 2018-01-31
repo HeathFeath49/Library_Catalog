89d282b79e931a22586858a258417ab21edf2983
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-01-30 19:29:06 -0500

    rearranged files so that App component is in own file and index.js is just used to render App component to DOM

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import MainView from './components/mainView';
+
+
+
+class App extends React.Component {
+
+	state = {
+		response: ''
+	};
+
+	componentDidMount(){
+		this.callApi()
+		.then(res => this.setState({ response: res.express }))
+		.catch(err => console.log(err));
+	}
+
+	callApi = async () => {
+		const response = await fetch('/api/hello');
+		const body = await response.json();
+
+		if(response.status !== 200){
+			throw Error(body.message);
+		}
+
+		return body;
+	};
+
+	render(){
+		return (
+			<div>
+				<MainView />
+			</div>
+		);
+	}
+
+}
+
+
+export default App;
