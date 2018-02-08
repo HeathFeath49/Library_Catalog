76cc13d9aaaa55221c176afd71fe2ebc8900c42e
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-02-07 21:28:13 -0500

    fixed empty req upon post request

--- src/js/server.js
+++ src/js/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
+const http = require('http').Server(app);
+//const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-
-
-//CONNECT TO DB
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
+const mysql = require('mysql');
+const connection = mysql.createConnection({
 	host:'localhost',
 	user:'duser',
 	password:'test',
@@ -14,11 +14,21 @@ var connection = mysql.createConnection({
 });
 
 
-connection.connect()
 
 
-app.get('/api/hello',(req,res) => {
-	res.send({express: 'Hell From Express'});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+/*connection.connect()*/
+
+
+app.post('/signUp',function(req,res){
+	/*console.log(req.body);*/
+	res.send(req.body);
 });
+
 
 app.listen(port, () => console.log('Server running'));
