8bcdd8ec3dd9de66bf871cc4852be7f20f06589d
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-02-11 21:18:29 -0500

    connection in own file

--- src/js/database/dbConnection.js
+++ src/js/database/dbConnection.js
@@ -13,16 +13,3 @@ connection.connect(function(err){
 module.exports = connection;
 
 
-/*var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : '',
-    database : 'chat'
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-});
-
-module.exports = connection;*/
