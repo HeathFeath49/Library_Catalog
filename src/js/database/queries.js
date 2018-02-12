940a07002272f5dd2fd28baa9f57949a7ded062f
HeathFeath49 <hstarkie@lions.molloy.edu>
2018-02-11 21:19:14 -0500

    registerUser query added, passwords hashed using bcrypt

--- src/js/database/queries.js
+++ src/js/database/queries.js
@@ -2,10 +2,23 @@ const mysql = require('mysql');
 const db = require('./dbConnection');
 
 
-module.export = {
+module.exports = {
 
 	registerUser:function(data,callback){
-
+		db.query('SELECT Username FROM users WHERE Username = ?',[data.username],function(err,result){
+			if(result == 0){
+				db.query('INSERT INTO users SET ?',data,function(err,result){
+					if(err){ 
+						return callback(err);
+					};
+					return callback(null,true);
+				})
+			}
+			else{
+				// have it direct back to signUp form with message
+				return callback(null,false);
+			}
+		})
 	}
 
 	
